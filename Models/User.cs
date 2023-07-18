be87658d2c09c49a3b37a3a4974f832e11b2a5a3
gsagar31 <gaurav.sagar98@gmail.com>
2023-07-18 15:08:21 +0530

    intial commit 2

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public long Salary { get; set; }
+        public string Designation { get; set; }
+    }
+}
